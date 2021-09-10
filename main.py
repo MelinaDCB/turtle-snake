8888ff902051a11abf09c0674d8e31310428455b
MelinaDCB <89150123+MelinaDCB@users.noreply.github.com>
2021-09-11 01:13:35 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,44 +4,57 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+def snake_game():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+    screen.tracer(0)
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
-    # Detect collision with food.
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.update_score()
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
-    # Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.game_over()
-        game_is_on = False
+        # Detect collision with food.
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.update_score()
 
-    # Detect collision with tail.
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
+        # Detect collision with wall.
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
             scoreboard.game_over()
             game_is_on = False
+            replay = screen.textinput("Replay", "Do you wanna play again ? Type 'Yes' or 'No' ").lower()
+            if replay == "yes":
+                screen.clear()
+                snake_game()
 
-            
+        # Detect collision with tail.
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                scoreboard.game_over()
+                game_is_on = False
+                replay = screen.textinput("Replay", "Do you wanna play again ? Type 'yes' or 'no' ").lower()
+                if replay == "yes":
+                    screen.clear()
+                    snake_game()
+
+
+snake_game()
+
+
 screen.exitonclick()
